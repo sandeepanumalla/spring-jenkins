ca62eaf2264c47b8f51183b14b3e820c5b5fcd33
Sandeep <anumalla.sandeep1999@gmail.com>
2023-08-27 22:08:05 +0530

    fixed the spring deploy issue

--- src/main/java/com/example/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/example/springjenkins/SpringJenkinsApplication.java
@@ -5,9 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringJenkinsApplication {
+public class SpringJenkinsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringJenkinsApplication.class);
+    }
 
     static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
